ecfe4f732841e0924845218c030ae7207bf41bb5
Yash Ghatti <yashghatti@gmail.com>
2020-06-16 12:25:17 -0600

    added ec2 service

---
+++ ec2-transaction-manager/src/main/java/ca/yashghatti/tm/Ec2TransactionManagerApplication.java
@@ -0,0 +1,13 @@
+package ca.yashghatti.tm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2TransactionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2TransactionManagerApplication.class, args);
+	}
+
+}
---
+++ ec2-transaction-manager/src/main/java/ca/yashghatti/tm/controllers/TransactionController.java
@@ -0,0 +1,15 @@
+package ca.yashghatti.tm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class TransactionController {
+
+    @GetMapping("/")
+    public String home() {
+        return "forward:/actuator/info";
+    }
+
+}
