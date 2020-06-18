b2538be44c32cde18e18f64086cc829d24139e33
Yash Ghatti <yashghatti@gmail.com>
2020-06-17 23:02:02 -0600

    minor change

--- ec2-transaction-manager/src/main/java/ca/yashghatti/tm/Application.java
+++ ec2-transaction-manager/src/main/java/ca/yashghatti/tm/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ec2TransactionManagerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ec2TransactionManagerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
