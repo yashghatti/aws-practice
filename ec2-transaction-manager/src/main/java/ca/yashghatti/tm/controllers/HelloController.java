61188e03b0d42d8657d3c1110caf45467b3263fa
Yash Ghatti <yashghatti@gmail.com>
2020-06-28 03:14:56 -0600

    Dockerfile running

--- ec2-transaction-manager/src/main/java/ca/yashghatti/tm/controllers/HelloController.java
+++ ec2-transaction-manager/src/main/java/ca/yashghatti/tm/controllers/HelloController.java
@@ -1,10 +1,12 @@
 package ca.yashghatti.tm.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
+    @GetMapping("/hello")
     public String helloWorld() {
         return "<h1>Hello World</h1>";
     }
